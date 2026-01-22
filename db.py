2f845722cb1e33cb416732dd86f7631e0c467e0e
JoeRojas1914 <joerojas1914@gmail.com>
2026-01-22 17:15:18 -0600

    Adicion de db.py

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
 
 def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="*Teddygringo1",
-        database="FreshSteps"
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME")
     )
